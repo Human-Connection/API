0c306b7262571031bb56f3c73f5aa94150108e70
JB <jb@visualjerk.de>
2017-07-02 00:03:47 +0200

    Basic file upload

---
+++ server/services/uploads/uploads.service.js
@@ -0,0 +1,50 @@
+// Initializes the `uploads` service on path `/uploads`
+const hooks = require('./uploads.hooks');
+const filters = require('./uploads.filters');
+
+// File Service
+// feathers-blob service
+const blobService = require('feathers-blob');
+// Here we initialize a FileSystem storage,
+// but you can use feathers-blob with any other
+// storage service like AWS or Google Drive.
+const fs = require('fs-blob-store');
+const blobStorage = fs(__dirname + '/../../../uploads');
+
+// For Multipart Uploads
+const multer = require('multer');
+const multipartMiddleware = multer();
+
+module.exports = function () {
+  const app = this;
+
+  const options = {
+    name: 'uploads',
+    Model: blobStorage
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/uploads',
+    // multer parses the file named 'uri'.
+    // Without extra params the data is
+    // temporary kept in memory
+    multipartMiddleware.single('uri'),
+
+    // another middleware, this time to
+    // transfer the received file to feathers
+    function(req,res,next){
+      req.feathers.file = req.file;
+      next();
+    },
+    blobService(options)
+  );
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('uploads');
+
+  service.hooks(hooks);
+
+  if (service.filter) {
+    service.filter(filters);
+  }
+};
