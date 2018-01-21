a2b9568c56d2e78ceb2630853601637c97c0a91d
Grzegorz Leoniec <greg@app-interactive.de>
2018-01-21 12:42:41 +0100

    enabled console logging on production

--- server/logger.js
+++ server/logger.js
@@ -9,22 +9,22 @@ const logger = createLogger({
     new transports.File({ filename: './data/combined.log' })
   ]
 });
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
-    format: format.combine(
-      format.printf((info) => {
-        const date = new Date();
-        const levelColors = {
-          info: '',
-          debug: '\u001b[34m',
-          warning: '\u001b[33m',
-          error: '\u001b[31m'
-        };
-        return `${levelColors[info.level]}${date.toLocaleTimeString()} | ${info.level}: ${JSON.stringify(info.message)}\u001b[39m`;
-      })
-    ),
-    level: 'debug'
-  }));
-}
+// if (process.env.NODE_ENV !== 'production') {
+logger.add(new transports.Console({
+  format: format.combine(
+    format.printf((info) => {
+      const date = new Date();
+      const levelColors = {
+        info: '',
+        debug: '\u001b[34m',
+        warning: '\u001b[33m',
+        error: '\u001b[31m'
+      };
+      return `${levelColors[info.level]}${date.toLocaleTimeString()} | ${info.level}: ${JSON.stringify(info.message)}\u001b[39m`;
+    })
+  ),
+  level: 'debug'
+}));
+// }
 
 module.exports = logger;
