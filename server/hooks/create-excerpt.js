8856252e8f7b60507c02732029e1d6fe66ecee9d
Grzegorz Leoniec <greg@app-interactive.de>
2018-02-20 00:30:57 +0100

    added better excerpts

--- server/hooks/create-excerpt.js
+++ server/hooks/create-excerpt.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 
 // https://github.com/yangsibai/node-html-excerpt
-const excerpt = require('html-excerpt');
+// const excerpt = require('html-excerpt');
+const trunc = require('trunc-html');
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
@@ -10,9 +11,17 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     }
     /* eslint no-use-before-define: 0 */  // --> OFF
     const content = hook.data.content
-      .replace(/\<br\>|\<\/br\>|\<\/ br\>|\<\/(strong|b|h[1-6])>/ig, "\n")
-      .replace(/(<([^>]+)>)/ig, '');
-    hook.data.contentExcerpt = excerpt.text(content, 120, '...');
+      .replace(/\<br\>|\<\/br\>|\<\/ br\>|\<br\>|\<br\\\>|\<p\>|\<\/p\>/ig, "\n")
+      .replace(/\<(strong|b|i|blockquote|pre|em|u|h[1-6])>|\<\/(strong|b|i|blockquote|pre|em|u|h[1-6])>/ig, '')
+      .replace(/\<p\>\<br\>\<\/p\>/ig, ' ')
+      .replace(/(\ )[2,]/ig, ' ')
+      .trim();
+    hook.data.contentExcerpt = trunc(content, 120, {
+      ignoreTags: ['img', 'script']
+    }).html;
+
+    hook.data.content = hook.data.content
+      .replace(/(\ )[2,]/ig, ' ')
     return Promise.resolve(hook);
   };
 };
