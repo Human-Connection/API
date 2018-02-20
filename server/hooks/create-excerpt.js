dc42f07e9dfd455ed789374ded12a41baced6190
Sebastian Koch <mimic@42geeks.gg>
2018-02-20 16:44:58 +0100

    Merge branches 'develop' and 'feature/NEXT-250' of https://team.human-connection.org/bitbucket/scm/hc/hc-api-feathers into feature/NEXT-250
    # Conflicts:
    #	server/hooks/create-excerpt.js

--- server/hooks/create-excerpt.js
+++ server/hooks/create-excerpt.js
@@ -4,23 +4,30 @@
 // const excerpt = require('html-excerpt');
 const trunc = require('trunc-html');
 
-module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
+module.exports = function (options = {
+  field: 'content',
+  length: 120
+}) { // eslint-disable-line no-unused-vars
   return function (hook) {
-    if(!hook.data || !hook.data.content) {
+    console.log('TRY TO CREATE EXCERPT');
+    console.log('field', options.field);
+    if(!options.field || !hook.data[options.field]) return hook;
+
+    if(!hook.data || !hook.data[options.field]) {
       return hook;
     }
     /* eslint no-use-before-define: 0 */  // --> OFF
-    const content = hook.data.content
+    const text = hook.data[options.field]
       .replace(/\<br\>|\<\/br\>|\<\/ br\>|\<br\>|\<br\\\>|\<p\>|\<\/p\>/ig, "\n")
       .replace(/\<(strong|b|i|blockquote|pre|em|u|h[1-6])>|\<\/(strong|b|i|blockquote|pre|em|u|h[1-6])>/ig, '')
       .replace(/\<p\>\<br\>\<\/p\>/ig, ' ')
       .replace(/(\ )[2,]/ig, ' ')
       .trim();
-    hook.data.contentExcerpt = trunc(content, 120, {
+    hook.data[`${options.field}Excerpt`] = trunc(text, options.length, {
       ignoreTags: ['img', 'script']
     }).html;
 
-    hook.data.content = hook.data.content
+    hook.data[options.field] = hook.data[options.field]
       .replace(/(\ )[2,]/ig, ' ')
     return Promise.resolve(hook);
   };
