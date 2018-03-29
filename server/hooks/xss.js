053f2adc5c87ec6ca8c60025fdca9cb94ee7735b
JB <jb@visualjerk.de>
2018-03-30 01:31:41 +0200

    update embed-to-anchor

--- server/hooks/xss.js
+++ server/hooks/xss.js
@@ -1,19 +1,10 @@
 const sanitizeHtml = require('sanitize-html');
-// const utils = require('quill-url-embeds');
+const embedToAnchor = require('quill-url-embeds/dist/embed-to-anchor');
 const _ = require('lodash');
-const cheerio = require('cheerio');
 
 function clean (dirty) {
   // Convert embeds to a-tags
-  // dirty = utils.embedToAnchor(dirty);
-  const $ = cheerio.load(dirty);
-  $('div[data-url-embed]').each((i, el) => {
-    let url = el.attribs['data-url-embed'];
-    let aTag = $(`<a href="${url}" target="_blank" data-url-embed>${url}</a>`);
-    $(el).replaceWith(aTag);
-  })
-  dirty = $('body').html();
-
+  dirty = embedToAnchor(dirty);
   dirty = sanitizeHtml(dirty, {
     allowedTags: ['iframe', 'img', 'p', 'br', 'b', 'i', 'em', 'strong', 'a', 'pre', 'ul', 'li', 'ol', 'span'],
     allowedAttributes: {
