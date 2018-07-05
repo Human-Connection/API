1ca5e0ae2d2049bdb3f06e5cbf59b11f8432a8e2
Grzegorz Leoniec <greg@app-interactive.de>
2018-07-05 12:33:01 +0200

    added settings service

--- server/services/index.js
+++ server/services/index.js
@@ -21,6 +21,7 @@ const search = require('./search/search.service.js');
 const usersettings = require('./usersettings/usersettings.service.js');
 const pages = require('./pages/pages.service.js');
 const systemNotifications = require('./system-notifications/system-notifications.service.js');
+const settings = require('./settings/settings.service.js');
 
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
@@ -47,4 +48,5 @@ module.exports = function () {
   app.configure(usersettings);
   app.configure(pages);
   app.configure(systemNotifications);
+  app.configure(settings);
 };
