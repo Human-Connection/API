db849beff17d60f61da83d59d5d4fe4b1ed1e08a
Grzegorz Leoniec <greg@app-interactive.de>
2018-07-06 14:58:31 +0200

    added onlyUserWithBadgesCanInvite setting

--- server/models/settings.model.js
+++ server/models/settings.model.js
@@ -10,7 +10,8 @@ module.exports = function (app) {
     key: {type: String, default: 'system', index: true, unique: true},
     invites: {
       userCanInvite: { type: Boolean, required: true, default: false },
-      maxInvitesByUser: { type: Number, required: true, default: 1 }
+      maxInvitesByUser: { type: Number, required: true, default: 1 },
+      onlyUserWithBadgesCanInvite: { type: Array, default: [] }
     },
     maintenance: false
   }, {
