69122a7817bf85a9812007959703e9037f677c24
Robert Schäfer <git@roschaefer.de>
2018-07-24 15:02:31 +0200

    Finished test setup to prevent a blacklisting for
    * yourself
    * a moderator
    * an admin user

--- test/services/usersettings.test.js
+++ test/services/usersettings.test.js
@@ -17,21 +17,89 @@ describe('\'usersettings\' service', () => {
     this.server.close(done);
   });
 
-  beforeEach(async () => {
-    await app.get('mongooseClient').connection.dropDatabase();
-    user = await userService.create(adminData);
-    params = {
-      user
-    };
-  });
-
-  afterEach(async () => {
-    await app.get('mongooseClient').connection.dropDatabase();
-    user = null;
-    params = null;
-  });
-
   it('registered the service', () => {
     assert.ok(service, 'registered the service');
+  });
+
+  context('given a user', () => {
+    beforeEach(async () => {
+      await app.get('mongooseClient').connection.dropDatabase();
+      user = await userService.create(userData);
+      params = {
+        user
+      };
+    });
+
+    afterEach(async () => {
+      await app.get('mongooseClient').connection.dropDatabase();
+      user = null;
+      params = null;
+    });
+
+    describe('create', () => {
+      it('succeeds', async () => {
+        let data = {
+          userId: user._id
+        };
+        const usersettings = await service.create(data);
+        assert.ok(usersettings);
+      });
+
+      describe.only('blacklist', () => {
+        const testSetup = async (blacklistedUserData) => {
+          let blacklistedUser = await userService.create({
+            ...userData,
+            ...blacklistedUserData,
+            email: 'someoneelse@test.de',
+            name: 'Someone'
+          });
+          let data = {
+            userId: user._id,
+            blacklist: [blacklistedUser._id]
+          };
+          const usersettings = await service.create(data);
+          return usersettings[0].blacklist;
+        };
+
+        context('ordinary account', () => {
+          it('succeeds', async () => {
+            const blacklist = await testSetup({});
+            assert.equal(blacklist.length, 1);
+          });
+        });
+
+        context('own account', () => {
+          it('rejects', async () => {
+            assert.throws(async () => {
+              let data = {
+                userId: user._id,
+                blacklist: [user._id]
+              };
+              const usersettings = await service.create(data);
+            });
+            const usersettings = await service.find({userId: user._id});
+            const blacklist = usersettings.data[0].blacklist;
+            assert.equal(blacklist.length, 0);
+          });
+        });
+
+        context('moderator account', () => {
+          it('rejects', async () => {
+            await testSetup({role: 'moderator'});
+            const usersettings = await service.find({userId: user._id});
+            const blacklist = usersettings.data[0].blacklist;
+            assert.equal(blacklist.length, 0);
+          });
+        });
+        context('admin account', () => {
+          it('rejects', async () => {
+            await testSetup({role: 'admin'});
+            const usersettings = await service.find({userId: user._id});
+            const blacklist = usersettings.data[0].blacklist;
+            assert.equal(blacklist.length, 0);
+          });
+        });
+      });
+    });
   });
 });
